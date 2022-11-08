3835fd82ce06a06fde3bd55bc5882be47cca9273
ahadulislampinoy <ahadulislampinoy@gmail.com>
2022-11-08 17:51:52 +0600

    single service page first half done

--- src/Pages/Service/Service.js
+++ src/Pages/Service/Service.js
@@ -1,51 +1,29 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import "./Sevice.css";
 
 const Service = () => {
   const service = useLoaderData();
-  console.log(service);
+  const { name, img, price, description } = service;
+
   return (
     <div>
-      <section>
-        <div class="mx-auto max-w-screen-2xl px-4 py-16 sm:px-6 lg:px-8">
-          <div class="grid grid-cols-1 lg:h-screen lg:grid-cols-2">
-            <div class="relative z-10 lg:py-16">
-              <div class="relative h-64 sm:h-80 lg:h-full">
-                <img
-                  alt="House"
-                  src="https://images.unsplash.com/photo-1583608205776-bfd35f0d9f83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                  class="absolute inset-0 h-full w-full object-cover"
-                />
-              </div>
-            </div>
-
-            <div class="relative flex items-center bg-gray-100">
-              <span class="hidden lg:absolute lg:inset-y-0 lg:-left-16 lg:block lg:w-16 lg:bg-gray-100"></span>
-
-              <div class="p-8 sm:p-16 lg:p-24">
-                <h2 class="text-2xl font-bold sm:text-3xl">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Tempore, debitis.
-                </h2>
-
-                <p class="mt-4 text-gray-600">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Aliquid, molestiae! Quidem est esse numquam odio deleniti,
-                  beatae, magni dolores provident quaerat totam eos, aperiam
-                  architecto eius quis quibusdam fugiat dicta.
-                </p>
-
-                <a
-                  href="#"
-                  class="mt-8 inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
-                >
-                  Get in Touch
-                </a>
-              </div>
-            </div>
+      <div className="p-6 sm:p-10 py-10">
+        <div className="lg:flex justify-between items-center">
+          <div className="lg:w-6/12 lg:p-0 px-2 py-7 text-gray-800">
+            <h1 className="text-5xl lg:text-6xl my-6 drop-shadow-md font-bold text-pink-500">
+              ${price}
+            </h1>
+            <h1 className="text-4xl font-bold leading-tight mb-5 capitalize">
+              {name}
+            </h1>
+            <p className="text-xl leading-8">{description}</p>
+          </div>
+          <div className="lg:w-2/6 order-2">
+            <img src={img} alt="" className="rounded service-img" />
           </div>
         </div>
-      </section>
+      </div>
     </div>
   );
 };
